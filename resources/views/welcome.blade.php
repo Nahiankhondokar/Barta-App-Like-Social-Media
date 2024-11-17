04cca13a515afe9ca1165ec6a6b6674e39d21e61
nahiankhondokar <nahiankhondokarphp@gmail.com>
2024-11-17 12:31:19 +0600

    project clean for barta app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,138 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Barta App</title>
 
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    <main>
-      <div class="header mb-5">
-        <nav class="navbar navbar-expand-lg navbar-light bg-info fixed-top ">
-          <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link fw-bold text-white rounded bg-primary m-1" href="#">Posts</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link fw-bold text-white rounded bg-primary m-1" href="#">Create Post</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+    <div class="container">
+        <div class="title text-center text-info">
+            <h1>Hello world</h1>
+        </div>
     </div>
-    
-        <div class="container">
-            <div class="row">
-              <div class="col-md-3 mt-5">
-                  <div class="card shadow-sm">
-                    <img src="{{asset('assets/image/new-post.png')}}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                  </div>
-              </div>
-
-              <div class="col-md-3 mt-5">
-                <div class="card shadow-sm">
-                  <img src="{{asset('assets/image/follow-us.png')}}" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-3 mt-5">
-                  <div class="card shadow-sm">
-                    <img src="{{asset('assets/image/new-post.png')}}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                  </div>
-              </div>
-
-              <div class="col-md-3 mt-5">
-                <div class="card shadow-sm">
-                  <img src="{{asset('assets/image/follow-us.png')}}" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-3 mt-5">
-                <div class="card shadow-sm">
-                  <img src="{{asset('assets/image/new-post.png')}}" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                  </div>
-                </div>
-            </div>
-
-            <div class="col-md-3 mt-5">
-              <div class="card shadow-sm">
-                <img src="{{asset('assets/image/follow-us.png')}}" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-3 mt-5">
-              <div class="card shadow-sm">
-                <img src="{{asset('assets/image/new-post.png')}}" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
-          </div>
-
-          <div class="col-md-3 mt-5">
-            <div class="card shadow-sm">
-              <img src="{{asset('assets/image/follow-us.png')}}" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-3 mt-5">
-            <div class="card shadow-sm">
-              <img src="{{asset('assets/image/new-post.png')}}" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-        </div>
-
-        <div class="col-md-3 mt-5">
-          <div class="card shadow-sm">
-            <img src="{{asset('assets/image/follow-us.png')}}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-            </div>
-                
-            </div>
-        </div> 
-    </main>
 </body>
 </html>
