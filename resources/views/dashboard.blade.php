9eedf69f9e728eb206ac381291c7eb5f92e496a1
nahiankhondokar <nahiankhondokarphp@gmail.com>
2024-11-24 21:35:41 +0600

    single post view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -224,17 +224,19 @@ class="container max-w-xl mx-auto space-y-8 mt-8 px-2 md:px-0 min-h-screen">
 
       <!-- Content -->
       <div class="py-4 text-gray-700 font-normal">
-        <p>
-          {{$post->barta}}
-          <br />
-          <a
-            href="#laravel"
-            class="text-black font-semibold hover:underline"
-            >#Laravel</a
-          >
-          <br />
-          <br />
-        </p>
+        <a href="{{route('post.show', $post->id)}}">
+          <p>
+            {{$post->barta}}
+            <br />
+            <a
+              href="#laravel"
+              class="text-black font-semibold hover:underline"
+              ></a
+            >
+            <br />
+            <br />
+          </p>
+        </a>
       </div>
 
       <!-- Date Created & View Stat -->
--- resources/views/post/single.blade.php
+++ resources/views/post/single.blade.php
@@ -30,13 +30,13 @@ class="container max-w-xl mx-auto space-y-8 mt-8 px-2 md:px-0 min-h-screen">
             <a
               href="profile.html"
               class="hover:underline font-semibold line-clamp-1">
-              Al Nahian
+              {{auth()->user()->username}}
             </a>
 
             <a
               href="profile.html"
               class="hover:underline text-sm text-gray-500 line-clamp-1">
-              @alnahian2003
+              {{auth()->user()->email}}
             </a>
           </div>
           <!-- /User Info -->
@@ -98,23 +98,23 @@ class="container max-w-xl mx-auto space-y-8 mt-8 px-2 md:px-0 min-h-screen">
     <!-- Content -->
     <div class="py-4 text-gray-700 font-normal">
       <p>
-        🎉🥳 Turning 20 today! 🎂
+        
         <br />
-        One of the best things in my life has been my love affair with
+        {{$post->barta}}
         <a
           href="#laravel"
           class="text-black font-semibold hover:underline"
-          >#Laravel</a
+          ></a
         >
         <br />
         <br />
-        Keep me in your prayers 😌
+       
       </p>
     </div>
 
     <!-- Date Created & View Stat -->
     <div class="flex items-center gap-2 text-gray-500 text-xs my-2">
-      <span class="">6 minutes ago</span>
+      <span class="">{{$post->created_at ? Carbon\Carbon::parse($post->created_at)->diffForHumans() : ''}}</span>
       <span class="">•</span>
       <span>450 views</span>
     </div>
