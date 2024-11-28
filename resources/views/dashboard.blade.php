e8102d69dd2fd709a36db3c97c8ba1503a0ae599
nahiankhondokar <info.nahian13@gmail.com>
2024-11-28 13:37:27 +0600

    header design load

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -242,7 +242,7 @@ class="container max-w-xl mx-auto space-y-8 mt-8 px-2 md:px-0 min-h-screen">
             <br />
           </p>
         </a>
-        <img src="{{asset($post->image)}}" alt="">
+        <img src="{{Storage::url($post->image)}}" alt="">
       </div>
 
       <!-- Date Created & View Stat -->
