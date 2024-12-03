faf6d2ca0fde564c427bd6b8fe2de86fc5b043db
nahiankhondokar <info.nahian13@gmail.com>
2024-12-03 19:05:46 +0600

    vue router install

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,10 @@ import './bootstrap';
 import { createApp } from 'vue';
 import App from './App.vue';
 import Posts from './components/Post/Posts.vue';
+import VueRouter from 'vue-router'
  
 const app = createApp();
 
+app.use(VueRouter)
 app.component('posts', Posts)
 app.mount('#app');
