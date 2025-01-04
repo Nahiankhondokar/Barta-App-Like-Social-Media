d0f1545e6eca9e0a285d54c030d1f2ceeab9ee02
nahiankhondokar <info.nahian13@gmail.com>
2025-01-04 17:49:56 +0600

    notification icon added

--- resources/js/components/Layouts/Header.vue
+++ resources/js/components/Layouts/Header.vue
@@ -93,6 +93,16 @@ onMounted(() => {
                     </form>
                     <div class="hidden sm:ml-6 sm:flex gap-2 sm:items-center">
                         <!-- This Button Should Be Hidden on Mobile Devices -->
+
+                        <div
+                            class="notification-icon text-xl text-gray-500 pr-6"
+                        >
+                            <a href="">
+                                <i class="fa-solid fa-bell"></i>
+                            </a>
+                            <!-- <span>1</span> -->
+                        </div>
+
                         <button
                             type="button"
                             class="text-gray-900 hover:text-white border-2 border-gray-800 hover:bg-gray-900 focus:ring-2 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center hidden md:block"
