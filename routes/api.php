f019ca23902f7d438e9ab23499820b04012ab22a
nahiankhondokar <info.nahian13@gmail.com>
2025-01-14 13:03:06 +0600

    cicd test pipeline

--- routes/api.php
+++ routes/api.php
@@ -38,5 +38,5 @@ Route::middleware('auth:sanctum')->group(function(){
 });
 
 Route::get('cicd-test', function(){
-    echo 'CICD test is successful';
+    echo 'CICD test is successful, Hello world';
 });
